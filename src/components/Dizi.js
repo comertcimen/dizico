e4fccfce38113b57a771e809ca15f239e105763e
ccimen <ccimen@mikrokom.com>
2021-05-05 21:34:07 +0300

    Adding local env support

--- src/components/Dizi.js
+++ src/components/Dizi.js
@@ -61,8 +61,7 @@ const Dizi = () => {
     const url = `https://api.themoviedb.org/3/tv/${tmdbid}`;
     const config = {
       headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNTU0OGI5ZmRkYzA1YTIzMzYwZTI1ZTgwOTQ1MDg0MSIsInN1YiI6IjVlY2Q1YTMxMDIxY2VlMDAyMThiOThkZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.A1j2NgaUXxYUeGy0uRMbeQSEziWq0GoGzL24KyYv3n8",
+        Authorization: process.env.REACT_APP_KEY,
         "Content-Type": "application/json;charset=utf-8",
       },
     };
